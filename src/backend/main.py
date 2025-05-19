788805f0dc0a578d65877be55ef9715a6395a8be
vitorlpx <lopessccp.3k@gmail.com>
2025-05-19 18:18:42 -0300

    feat: adicionando o sqs na aplicação

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import estatistica, farmtech, predicao
+from routers import estatistica, farmtech, predicao, simulador_esp32
 
 app = FastAPI()
 
@@ -7,3 +7,4 @@ app = FastAPI()
 app.include_router(estatistica.router, prefix="/v1/farmtech/analises/r")
 app.include_router(farmtech.router, prefix="/v1/farmtech")
 app.include_router(predicao.router, prefix="/v1/farmtech")
+app.include_router(simulador_esp32.router, prefix="/v1/farmtech")
